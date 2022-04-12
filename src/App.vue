24678a851a6a7d9db81c6b200a3b3cc74c98619a
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-12 14:12:00 +0530

    signupform added to app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="my-24 flex flex-col justify-center align-middle lg:flex-row">
-    <SubjectCard></SubjectCard>
+  <div class="flex flex-col justify-center align-middle">
+    <div class="container">
+      <SignUpForm></SignUpForm>
+    </div>
+    <div
+      class="my-24 flex flex-1 flex-col justify-center align-middle lg:flex-row"
+    >
+      <SubjectCard></SubjectCard>
+    </div>
   </div>
 </template>
 
 <script>
 import SubjectCard from "./components/SubjectCard.vue";
+import SignUpForm from "./components/SignUpForm.vue";
+import SignUpForm1 from "./components/SignUpForm.vue";
 export default {
   // name:"App",
   components: {
     SubjectCard,
+    SignUpForm,
+    SignUpForm1,
   },
 };
 </script>
