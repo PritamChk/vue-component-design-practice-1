31234f8dea49091b2dac72edeacdc4cd98568287
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-12 18:53:14 +0530

    form motamuti ready

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -9,11 +9,23 @@
       <h1 class="form-header col-span-2 ml-2 self-center">Name</h1>
       <div class="col-span-2 md:col-span-1">
         <label class="label" for="fname">First: </label>
-        <input class="input-box" type="text" placeholder="Tatha" required />
+        <input
+          class="input-box"
+          name="fname"
+          type="text"
+          placeholder="Tatha"
+          required
+        />
       </div>
       <div class="col-span-2 md:col-span-1">
-        <label class="label" for="fname">Last: </label>
-        <input class="input-box" type="text" placeholder="Gata" required />
+        <label class="label" for="lname">Last: </label>
+        <input
+          class="input-box"
+          name="lname"
+          type="text"
+          placeholder="Gata"
+          required
+        />
       </div>
     </div>
     <div
@@ -21,17 +33,19 @@
     >
       <h1 class="form-header col-span-2 ml-2 self-center">Credentials</h1>
       <div class="col-span-2 md:col-span-1">
-        <label class="label" for="fname">Email: </label>
+        <label class="label" for="email">Email: </label>
         <input
           class="input-box"
           type="email"
+          name="email"
           placeholder="tatha@gata.com"
           required
         />
       </div>
       <div class="col-span-2 md:col-span-1">
-        <label class="label" for="fname">Password: </label>
+        <label class="label" for="passwd">Password: </label>
         <input
+          name="passwd"
           class="input-box"
           type="password"
           placeholder="******"
@@ -43,7 +57,7 @@
     </div>
 
     <div
-      class="mt-6 flex flex-col space-y-2 md:space-y-0 md:flex-row md:justify-evenly md:space-x-2 md:align-baseline"
+      class="mt-6 flex flex-col space-y-2 md:flex-row md:justify-evenly md:space-y-0 md:space-x-2 md:align-baseline"
     >
       <input
         class="flex-1 rounded-3xl bg-lime-300 py-2 px-10 text-xl font-bold text-black hover:bg-lime-200"
