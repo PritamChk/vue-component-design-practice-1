465bb411655a1b26878926058bcc5c994645d089
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-12 17:19:58 +0530

    form in progress once again

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,27 +1,46 @@
 <template>
-  <form
-    class="container mx-auto h-4/5 w-3/4 text-sm md:text-lg"
-    id="SignUpForm"
-  >
-    <!-- name group -->
-    <div class="flex flex-col justify-center">
-      <div class="mx-1 my-1 flex-1 px-2 py-2 text-left text-lg font-medium">
-        Name:
+  <form action="javascript:void(0)" 
+  class="flex flex-col text-gray-600">
+    <div class="grid grid-flow-col grid-cols-2 grid-rows-3 gap-2">
+      <h1 class="cols-span-2">Name</h1>
+      <div class="col-span-1">
+        <label for="fname">First: </label>
+        <input class="input-box" type="text" placeholder="Tatha" required />
       </div>
-      <div class="flex flex-wrap justify-around md:space-x-3 md:align-top">
-        <!-- fname -->
-        <div class="my-2 mx-2 flex-1 p-2">
-          <label for="first_name"> First</label>
-          <input type="text" name="first_name" id="" placeholder="Selena" />
-        </div>
-        <!-- lname -->
-        <div class="my-2 mx-2 flex-1 p-2">
-          <label for="first_name"> Last </label>
-          <input type="text" name="first_name" id="" placeholder="Gomez" />
-        </div>
+      <div class="col-span-1">
+        <label for="fname">Last: </label>
+        <input class="input-box" type="text" placeholder="Gata" required />
       </div>
     </div>
-    <!-- end of name group -->
+    <div>
+      <h1>Credentials</h1>
+      <div>
+        <label for="email">Email</label>
+        <input class="input-box" type="email" autocomplete="on" required />
+      </div>
+      <div>
+        <label for="fname">Password</label>
+        <input
+          class="input-box"
+          type="password"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+          title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+          required
+        />
+      </div>
+    </div>
+    <div>
+      <input
+        class="input-box hover:bg-lime-300 hover:text-black"
+        type="submit"
+        value="Submit"
+      />
+      <input
+        class="input-box hover:bg-pink-400 hover:text-black"
+        type="reset"
+        value="Clear"
+      />
+    </div>
   </form>
 </template>
 
