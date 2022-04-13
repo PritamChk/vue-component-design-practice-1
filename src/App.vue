ef23f67c5ba23578f8f7a41f841fe587073fe6af
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-13 12:02:42 +0530

    navbar not working

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="flex flex-col justify-center align-middle">
-    <div
-      class="mx-auto mt-10 h-4/5 w-3/5 rounded-md p-4 shadow-md shadow-fuchsia-500"
-    >
-      <SignUpForm></SignUpForm>
+    <div class="min-w-full sticky top-0">
+      <NavBar></NavBar>
     </div>
     <div
       class="my-24 flex flex-1 flex-col justify-center align-middle lg:flex-row"
     >
       <SubjectCard></SubjectCard>
+    </div>
+    <div
+      class="mx-auto mt-10 h-4/5 w-3/5 rounded-md p-4 shadow-md shadow-fuchsia-500"
+    >
+      <SignUpForm></SignUpForm>
     </div>
   </div>
 </template>
@@ -17,12 +20,14 @@
 import SubjectCard from "./components/SubjectCard.vue";
 import SignUpForm from "./components/SignUpForm.vue";
 import SignUpForm1 from "./components/SignUpForm.vue";
+import NavBar from "./components/NavBar.vue";
 export default {
   // name:"App",
   components: {
     SubjectCard,
     SignUpForm,
     SignUpForm1,
+    NavBar,
   },
 };
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="absolute top-0 h-full w-full bg-gray-400/80  backdrop-filter backdrop-blur-2xl">
+    <!-- <div
+      class=""
+    ></div> -->
+    <ul
+      class="relative flex flex-col justify-evenly text-lg font-semibold text-white opacity-100 md:flex-row"
+    >
+      <li
+        class="m-1 rounded-sm p-2 text-blue-400 hover:bg-slate-500 hover:text-fuchsia-200"
+      >
+        Link 1
+      </li>
+      <li
+        class="m-1 rounded-sm p-2 text-blue-400 hover:bg-slate-500 hover:text-fuchsia-200"
+      >
+        Link 2
+      </li>
+      <li
+        class="m-1 rounded-sm p-2 text-blue-400 hover:bg-slate-500 hover:text-fuchsia-200"
+      >
+        Link 3
+      </li>
+      <li
+        class="m-1 rounded-sm p-2 text-blue-400 hover:bg-slate-500 hover:text-fuchsia-200"
+      >
+        Link 4
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
