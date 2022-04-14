24f1843c173f97f00ae1c159c1726f59a1d2ad64
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-14 12:48:15 +0530

    flex adjusted

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav
     class="sticky top-0 z-20 bg-gradient-to-br from-slate-200/20 to-slate-100/10 shadow-md backdrop-blur-sm backdrop-filter"
   >
-    <ul class="flex flex-row items-center justify-evenly p-2 align-middle">
+    <ul class="flex flex-wrap flex-col md:flex-row items-center justify-evenly p-2 align-middle">
       <li
         class="hover: rounded px-5 py-1 backdrop-blur-xl transition-all duration-100 ease-in-out hover:bg-slate-50/50 hover:drop-shadow-xl hover:backdrop-filter"
       >
