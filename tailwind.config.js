473ec79a988030e442d518e3f0fd87d85d080107
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-16 11:18:32 +0530

    headlessUI & DaisyUI installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,35 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js}",
-    "./src/*.js"
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js}", "./src/*.js"],
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        onedark: {
+          primary: "#efea45",
+
+          secondary: "#fef9c3",
+
+          accent: "#2168c4",
+
+          neutral: "#171821",
+
+          "base-100": "#1c1917",
+
+          info: "#5695C8",
+
+          success: "#21C07B",
+
+          warning: "#AA6408",
+
+          error: "#EF6252",
+        },
+      },
+    ],
+  },
   plugins: [
-    require('tailwind-scrollbar-hide')
+    require("tailwind-scrollbar-hide"),
+    require("daisyui")
   ],
-}
+};
