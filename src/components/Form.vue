32180a200097f0074073304a6033f9b023483e55
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-16 14:38:44 +0530

    uninstalled headlessUI

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,5 @@
 <template>
-  <form
-    class="form"
-  >
+  <form class="form">
     <div class="form-section">
       <label class="label" for="fname">First Name</label>
       <input type="text" placeholder="Type here" class="input-blur" />
@@ -22,12 +20,12 @@
         max="30"
       />
     </div>
+  
     <div class="form-section">
-      <button type="submit" class="button hover:bg-gradient-to-tl from-lime-400/50 to-green-200/25">Submit</button>
-      <button type="reset" class="button hover:bg-gradient-to-tl from-red-400/40 to-pink-200/50">Reset</button>
+      <button type="submit" class="button button-success">Submit</button>
+      <button type="reset" class="button button-danger">Reset</button>
     </div>
   </form>
-
 </template>
 
 <script>
